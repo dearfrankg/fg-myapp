bea84e4b35bf6fafff662b1a077df4e4048d5d97
Frank Gutierrez <dearfrankg@gmail.com>
2017-08-07 22:15:53 -0700

    add export forwarding example

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { foo, msg1, msg2 } from "../config";
 
 export default class App extends React.Component {
   render() {
     return (
       <div style={{ textAlign: "center" }}>
         <h1>Hello World</h1>
+
+        <h3>Config Info</h3>
+        <p>
+          foo: {foo()}
+        </p>
+        <p>
+          msg1: {msg1}
+        </p>
+        <p>
+          msg2: {msg2}
+        </p>
       </div>
     );
   }
